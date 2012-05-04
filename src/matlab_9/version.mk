f7b4fc4fb28e8c782cc871249bcd778e5a524423
Jerry Greeenberg <jpg@sdsc.edu>
2012-05-04 22:16:38 +0000

    9 (+ dummy) packages

---
+++ src/matlab_9/version.mk
@@ -0,0 +1,5 @@
+NAME    = matlab_9
+VERSION = 2011b
+RELEASE = 1
+RPM.EXTRAS = "Autoprov: 0"
+
--- version.mk
+++ version.mk
@@ -1,5 +1,5 @@
 ROLLNAME = matlab
-RELEASE  = 0
+RELEASE  = 1
 COLOR    = orange
 
 REDHAT.ROOT = $(PWD)
