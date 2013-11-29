b8e34d9b1c26e3bf6ab24d07ac49186ebb32e291
Trevor Cooper <tcooper@sdsc.edu>
2013-11-29 14:01:20 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/matlab/version.mk
+++ src/matlab/version.mk
@@ -1,5 +1,15 @@
-NAME    = matlab
-VERSION = 2013a
-RELEASE = 7
-RPM.EXTRAS = "Autoprov: 0"
+NAME               = matlab
+VERSION            = 2013a
+RELEASE            = 7
+RPM.EXTRAS         = "Autoprov: 0"
+PKGROOT            = /opt/$(NAME)/$(VERSION)
 
+SRC_SUBDIR         = matlab
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
