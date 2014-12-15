4d9d8f2004cc14b8cbc158be1d007ad645730368
Jerry Greenberg <jpg@gcn-20-88.sdsc.edu>
2014-12-14 19:56:58 -0800

    got rid of extra rpm instruction

--- src/matlab_7/version.mk
+++ src/matlab_7/version.mk
@@ -4,4 +4,3 @@ VERSION = 2014b
 RELEASE = 1
 RPM.EXTRAS = "Autoprov: 0"
 RPM.EXTRAS = AutoReq:No
-RPM.EXTRAS = %define __os_install_post %{nil}
--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -3,4 +3,3 @@ PKGROOT = /root/rolltests
 VERSION	= 1
 RELEASE = 10
 RPM.EXTRAS = AutoReq:No
-RPM.EXTRAS     = %define _\_os\_install_post %{nil}
