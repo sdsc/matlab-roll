81047f10dee20d86fa6cbc1adf6fbb6da417b71f
Jerry Greenberg <jpg@sdsc.edu>
2018-03-16 18:44:10 -0700

    APS-743 modified matlab.t to work on v7

--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -2,7 +2,7 @@ NAME	   = sdsc-matlab-roll-test
 PKGROOT    = /root/rolltests
 PACKAGE    = matlab
 VERSION	   = 2
-RELEASE    = 1
+RELEASE    = 2
 
 RPM.EXTRAS = AutoReq:No\nAutoProv:No
 RPM.FILES  = $(PKGROOT)/matlab.t
