b009aca560f5af35c6b5e6ec95b10e4f76e8227c
Jerry Greenberg <jpg@sdsc.edu>
2018-07-26 08:24:03 -0700

    APS-823 make separate mdcs script package

--- src/matlab-mdcs-scripts/version.mk
+++ src/matlab-mdcs-scripts/version.mk
@@ -1,13 +1,11 @@
 PACKAGE     = matlab
-
-NAME        = sdsc-matlab
-VERSION     = 2018a
-RELEASE     = 1
-PKGROOT     = /opt/matlab/$(VERSION)
+NAME        = sdsc-matlab-mdcs-scripts
+PKGROOT     = /opt/matlab/$(VERSION)/toolbox/local/mdcs_files
+RELEASE     = 0
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
 RPM.EXTRAS  = AutoReq:No\nAutoProv:No
-RPM.PREFIX  = $(PKGROOT)
+RPM.FILES   = $(PKGROOT)
